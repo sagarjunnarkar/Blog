3b5de1a871bbf20e7fe1412d45476ede4ce18549
Sagar Junnarkar <sj@Sagars-iMac.local>
2013-12-19 19:41:52 +0530

    First commit to blog project
    Post scaffold added with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'd80cfe824b901eeb396b8581a395e2f4d0532f0767014e9eb5d9aa3ba2715419ba90b5aa64d8bfc7891d77a5364079f67cae01ad47a4016a87791706453a2225'
